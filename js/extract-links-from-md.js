fd2564b4b8367704a7488b1d411d646940939774
Ambar de Alejandría <antestumejorsonrisa@gmail.com>
2018-07-17 15:32:34 -0500

    Se modifica la expresión regular y la función pero sigue agregando la imagen

--- js/extract-links-from-md.js
+++ js/extract-links-from-md.js
@@ -3,31 +3,57 @@
 let glinks = [];
 let pairTextLink = {};
 
+
+
 const extractLinksFromMd = function (str)  {
 
-    const expRegular = /\[(\w.*)\]\((\w.*|(http(s)?|ftp:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&\/\/=]*)?)\)/gi;
-
+    toString(str);
+    const expRegular = /!?\[([^\[.]*?)\]\((.*?)\)/gi;
+    // const expRegular = /\[(\w.*)\]\((\w.*|(http(s)?|ftp:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&\/\/=]*)?)\)/gi;
+    
+    
     let match;
     while(match = expRegular.exec(str)){
+        if (match[1].startsWith("![")){
+        console.log(`Lo sentimos, esto ${match[1]} no es un link, es una imagen`);
+    } else{
         pairTextLink = {text: match[1], href: match[2]};
         glinks.push(pairTextLink);
     }
+        
+       
+    }//cierra while
  
-    // console.log("2", glinks);
+ 
 
     return glinks;
 
 
-}
+}//cierra extractLinksFromMd
+
+
+
+
 module.exports = extractLinksFromMd;
 
 
 })();
 
 
-// DOCUMENTACIÓN EXPRESIONES REGUALARES
 
-// /* \w -Coincide con cualquier carácter alfanumérico, incluyendo el guión bajo. Equivalente a [A-Za-z0-9_].
-//     . -Coincide con cualquier carácter precedente excepto un carácter de nueva línea.
-//     * -Busca el carácter precedente 0 (cero) o más veces. Es equivalente a {0,}.
-//     *\
+
+
+ /*   
+ ------------DOCUMENTACIÓN EXPRESIONES REGUALARES--------------------------
+
+       (x)  --Paréntesis de captura. Buscan 'x' y recuerdan la búsqueda. 
+       \w   --Coincide con cualquier carácter alfanumérico, incluyendo el guión bajo. Equivalente a [A-Za-z0-9_].
+       .    --Coincide con cualquier carácter precedente excepto un carácter de nueva línea.
+       *    --Busca el carácter precedente 0 (cero) o más veces. Es equivalente a {0,}.
+       []   --Grupo de caracteres. Este tipo de patrón coincide con cada carácter dentro de los corchetes, incluyendo secuencias de escapado. Caracteres especiales como el punto (.) y el asterisco (*) no son especiales en un grupo, así que no necesitan ser escapados.
+                Puede especificar un rango utilizando un guión, como en el siguiente ejemplo.
+       [^]  --Grupo de caracteres negativo. Significa que coincide con cualquier cosa que no esté en los corchetes.
+                Puede especificar rangos. Todo lo que funciona en el grupo de caracteres positivo funciona también aquí.
+       
+
+*/
