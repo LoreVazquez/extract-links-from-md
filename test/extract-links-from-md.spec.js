f7edebcf11ace89fd47ce480c843eea54c6a0261
Ambar de Alejandría <antestumejorsonrisa@gmail.com>
2018-07-17 18:39:11 -0500

    Se agrega el uso estricto de Js

--- test/extract-links-from-md.spec.js
+++ test/extract-links-from-md.spec.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const assert = require ('assert');
 const extractLinksFromMd = require('../js/extract-links-from-md');
 const str = `[enlaces locales al principio de texto sin caracter previo que puede romper la validación de imagen](local.md) este corchete  "[" que no debería alterar nada, el texto de [este enlace](http://enlace.com) aquí hay más texto(muy lame pero este parentesís puede provocar que truene la expresión regular).
@@ -11,4 +13,4 @@ const links = extractLinksFromMd(str);
 
 
 
-console.log("1",links);
+console.log(links);
