1586b971814f48b7fae7d66cad397e7d0fd374e6
Ambar de Alejandría <antestumejorsonrisa@gmail.com>
2018-07-19 11:03:03 -0500

    Se corrige la función demoGlinks, la cual ya imprime los valores.

--- js/demo.js
+++ js/demo.js
@@ -1,42 +1,26 @@
 'use strict';
+
 const demoGlinksMd = function() {
     let markDownText = $('#textarea1').val();
-
     let links = extractLinksFromMd(markDownText);
 
     links.forEach(element => {
-      let a = element;
-      console.log(element);
-      $('.card-panel').append(`<div class="white-text">${a}</div>`)
+
+      let text = element.text;
+      let href = element.href;
+    
+      $('#content').append(`
+          <p class="white-text"><strong>Text :</strong> ${text}</p>
+          <p><strong>href :</strong> ${href}</p>
+          <hr>
+      `);
     });
-
-    // console.log("v", extractLinksFromMd)
-    
-    // $('.card-panel').html(`<span class="white-text">${extractLinksFromMd(markDownText)}
-    // </span>`);
-    
-    // console.log(extractLinksFromMd(markDownText));
-    
-
-    
-
- 
   };
 
-// const gettingTextAreaValue = () => {
-//     const markDownText = $('#textarea1').val();
-//     return mkToArray(markDownText);
-// }
-
-// const mkToArray = (markDownText) => {
-//     console.log(extractLinksFromMd(markDownText));
-// }
 
 $(document).ready(function() {
 
       $('.btn').click(demoGlinksMd);
-    // $('.btn').click(mkToArray);
  
-        
  });
  
